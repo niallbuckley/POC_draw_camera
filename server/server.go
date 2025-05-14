c52d7613a368e1fa8ccc170705d9b92b003cacf5
Niall Buckley <buckleyniall100@gmail.com>
2025-05-14 14:27:16 +0200

    export data from json format into a interface

--- server/server.go
+++ server/server.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"fmt"
+	// "io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+
+
+func basicGetRequest(c echo.Context) error {
+     return c.JSON(http.StatusOK, map[string]string{"message": "Hello from Go Echo!",})
+}
+
+type sheetData struct {
+	Azimuth string `json:"azimuth"`
+	Fov int `json:"fov"`
+	Height int `json:"height"`
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
+	Tilt int `json:"tilt"`
+}
+
+func uploadSheetData(c echo.Context) error {
+	var data sheetData
+
+	if err := c.Bind(&data); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	 }
+	 fmt.Printf("%+v", data)
+
+	 return nil
+}
 
 func main() {
 	e := echo.New()
@@ -13,17 +41,9 @@ func main() {
 	// Enable CORS for all origins (for local dev)
 	e.Use(middleware.CORS())
 
-	// OR to restrict CORS:
-	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//     AllowOrigins: []string{"http://localhost:3000"},
-	//     AllowMethods: []string{http.MethodGet, http.MethodPost},
-	// }))
 
-	e.GET("/api/hello", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, map[string]string{
-			"message": "Hello from Go Echo!",
-		})
-	})
+	e.GET("/api/hello", basicGetRequest)
+	e.POST("/api/sheetData", uploadSheetData)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
